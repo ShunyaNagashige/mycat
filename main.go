f7c6920885a5c002b8195db372ac12cdc14e7f47
ShunyaNagashige <nagashige190441091@gmail.com>
2021-02-23 00:40:56 +0900

    オプションnの処理を追加

--- main.go
+++ main.go
@@ -7,6 +7,25 @@ import (
 	"os"
 )
 
+var isFlagNSelected bool
+var lineNum int = 1
+
+func init(){
+	usage:="行番号を表示させたい場合は、このオプションを選択してください。"
+	flag.BoolVar(&isFlagNSelected,"n",false,usage)
+}
+
+func main() {
+	//オプションの値を、コマンドから取得し設定する
+	flag.Parse()
+
+	for _, fn := range flag.Args() {
+		if err := readFile(fn); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+
 func readFile(fn string) error {
 	//sf=sourcefile(送り元、つまり読み込むファイル),fn=filename
 	sf, err := os.Open(fn)
@@ -18,6 +37,11 @@ func readFile(fn string) error {
 
 	//１行ずつ読み込んで繰り返す
 	for bsf.Scan() {
+		//オプションnを選択した場合、行番号を表示
+		if(isFlagNSelected){
+			fmt.Print(lineNum,": ")
+			lineNum++
+		}
 		//１行分を出力する
 		fmt.Println(bsf.Text())
 	}
@@ -28,15 +52,4 @@ func readFile(fn string) error {
 	}
 
 	return nil
-}
-
-func main() {
-	//オプションの設定
-	flag.Parse()
-
-	for _, fn := range flag.Args() {
-		if err := readFile(fn); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	}
 }
